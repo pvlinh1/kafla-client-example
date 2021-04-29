4b7a6529680df4f396ffe04f256314e21536481e
LDAP\pvlinh1 <pvlinh1@cmc.com.vn>
2021-04-29 17:08:37 +0700

    cádasdsad

---
+++ src/routers/user-router.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+import CartList from '../components/cart/Cart_List.vue';
+import ProductList from '../components/product/Product_List.vue';
+
+const routes = [
+  {
+    path: '/inventory',
+    component: ProductList
+  },
+  {
+    path: '/cart',
+    component: CartList
+  },
+  {
+    path: '/',
+    redirect: '/inventory'
+  },
+]
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
